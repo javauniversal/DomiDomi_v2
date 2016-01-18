2c933710c537dd1269005465f32ce218d0912798
servintesas <xp.sistem@hotmail.com>
2016-01-18 07:32:24 -0500

    Cambio de nombre

--- app/src/main/java/com/appgestor/domidomi/Adapters/AdapterComentario.java
+++ app/src/main/java/com/appgestor/domidomi/Adapters/AdapterComentario.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
 import com.appgestor.domidomi.Entities.ListComentarios;
 import com.appgestor.domidomi.R;
 
